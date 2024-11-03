dc10c55728081b47452e9c533dabc522d7308333
Y <87462160+harry585858@users.noreply.github.com>
2024-11-04 08:04:05 +0900

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() =>{
-    chrome.scripting.registerContentScripts([{
-        id:"remove-css",
-        matches:["<all_urls>"],
-        js:["contents.js"],
-        runAt: "document_idle"
-    }]);
-}
-)
