e2bc5fcf459296d8921d82cf71fb99b0694895c2
Y <87462160+harry585858@users.noreply.github.com>
2024-11-04 08:06:05 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -9,5 +9,12 @@ document.getElementById("runScriptButton").addEventListener("click", async () =>
 
 function yourFunction() {
   // 여기에서 JavaScript 코드를 작성합니다.
-  alert("JavaScript 코드가 실행되었습니다!");
+  StudyRecordDTO.studyTotalTime = 6974;
+StudyRecordDTO.webSessionTime = 6974;
+LessonWork.editStudyRecord (StudyRecordDTO, learningControl, 1, editStudyRecordCallback);
+//기간 후 수강시간 변경
+StudyRecordDTO.studyAfterTime = 0;
+StudyRecordDTO.webAfterTime = 0;
+LessonWork.editStudyRecord(StudyRecordDTO, learningControl, O, editStudyRecordCallback);
+  alert("실행되었습니다!");
 }
