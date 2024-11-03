509673b3c411b7a176cadabfd1e962b7b9a6ab1e
Y <87462160+harry585858@users.noreply.github.com>
2024-11-04 08:01:46 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
-console.log('no ads');
+document.getElementById("runScriptButton").addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: yourFunction
+  });
+});
+
+function yourFunction() {
+  // 여기에서 JavaScript 코드를 작성합니다.
+  alert("JavaScript 코드가 실행되었습니다!");
+}
