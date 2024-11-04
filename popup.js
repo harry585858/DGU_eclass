630e966a1af57adbdd8171258eda0114c8d6cdb5
Y <87462160+harry585858@users.noreply.github.com>
2024-11-04 09:13:02 +0900

    popup.js 업데이트

--- popup.js
+++ popup.js
@@ -9,12 +9,19 @@ document.getElementById("runScriptButton").addEventListener("click", async () =>
 
 function yourFunction() {
   // 여기에서 JavaScript 코드를 작성합니다.
-  StudyRecordDTO.studyTotalTime = 1;
-StudyRecordDTO.webSessionTime = 1;
-LessonWork.editStudyRecord (StudyRecordDTO, learningControl, 1, editStudyRecordCallback);
+ var inp = parseInt(prompt("시청시간 초단위 입력"));
+
+if (isNaN(inp)) {
+    alert("올바른 숫자를 입력해 주세요.");
+} else {
+    StudyRecordDTO.studyTotalTime = inp;
+    StudyRecordDTO.webSessionTime = inp;
+    LessonWork.editStudyRecord (StudyRecordDTO, learningControl, 1, editStudyRecordCallback);
 //기간 후 수강시간 변경
-StudyRecordDTO.studyAfterTime = 0;
-StudyRecordDTO.webAfterTime = 0;
-LessonWork.editStudyRecord(StudyRecordDTO, learningControl, O, editStudyRecordCallback);
-  alert("실행되었습니다!");
+    StudyRecordDTO.studyAfterTime = 0;
+    StudyRecordDTO.webAfterTime = 0;
+    LessonWork.editStudyRecord(StudyRecordDTO, learningControl, O, editStudyRecordCallback);
+    alert("실행되었습니다!");
+}
+  
 }
